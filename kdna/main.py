0f7ec4e5fd42e82ef7408b76c3e0196aff429401
pauline <pauline.contat00@gmail.com>
2023-12-12 09:15:26 +0100

    POC-4-PCO first commit passes tox

--- kdna/main.py
+++ kdna/main.py
@@ -1,10 +1,45 @@
-"""Main entry point for kdna"""""
+"""Main : Commande pour afficher le contenu d'un fichier"""
+import click
+
+
+def display(path):
+    """display : Affiche le contenu d'un fichier"""
+    try:
+        with open(path, mode="r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError as exc:
+        raise FileNotFoundError("Fichier non trouvé") from exc
+
+    except PermissionError as exc:
+        raise PermissionError("Oups, Pas les droits") from exc
+
+
+@click.group()
+def kdna():
+    """kdna : Commande principale"""
+
+
+@kdna.group()
+def backup():
+    """backup: Commande pour sauvegarder un fichier"""
+
+
+
+@backup.command()
+@click.option('-t', '--namepath', nargs=2, help="entrer le nom "
+                                                "et le chemin du fichier à sauvegarder")
+def add(namepath):
+    """add: Commande pour simuler l'ajout d'un fichier à la sauvegarde
+        -t, --namepath: option pour entrer le nom et le chemin du fichier à sauvegarder"""
+    name, path = namepath
+    click.echo(f"Created backup \"{name}\":v1.1.2")
+    click.echo("Contains :")
+    click.echo(display(path))
 
 
 def main():
-    """Main entry point for kdna"""""
-    print("Hello DO")
-
+    """main: Fonction principale"""
+    kdna()
 
 if __name__ == '__main__':
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,17 @@
-from kdna import main
+import click
+from click.testing import CliRunner
 
 
-def test_main():
-    assert main.main() is None
+def test_greet():
+    @click.command()
+    @click.argument('name')
+    def greet(name):
+        click.echo('Hello %s' % name)
+
+    runner = CliRunner()
+    result = runner.invoke(greet, ['Sam'])
+    assert result.output == 'Hello Sam\n'
+
+
+if __name__ == '__main__':
+    test_greet()
