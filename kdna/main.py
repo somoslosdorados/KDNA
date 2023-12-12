2ece40c3e42627c49c2858c2d0d409709c6ac7d1
pauline <pauline.contat00@gmail.com>
2023-12-12 20:14:47 +0100

    POC-4-PCO ajout auto backup et server

--- kdna/main.py
+++ kdna/main.py
@@ -24,6 +24,15 @@ def backup():
     """backup: Commande pour sauvegarder un fichier"""
 
 
+@kdna.group()
+def server():
+    """server: Commande pour lancer le serveur"""
+
+
+@kdna.group()
+def autobackup():
+    """backup: Commande pour mettre en place un daemon de sauvegarde"""
+
 
 @backup.command()
 @click.option('-t', '--namepath', nargs=2, help="entrer le nom "
@@ -37,9 +46,44 @@ def add(namepath):
     click.echo(display(path))
 
 
+@server.command()
+@click.argument('name')
+def set(name):
+    """set: Commande pour sélectionner un serveur
+        --name: option pour entrer le nom du serveur"""
+    click.echo(f"Picked server : \"{name}\"")
+
+
+@autobackup.command()
+@click.option('-n', '--nameofcron', nargs=1, help="entrer le nom du cron")
+@click.option('-t', '--tag', nargs=1, help="entrer le tag")
+@click.argument('cron_schedule', type=click.Choice(['daily', 'monthly', 'weekly',
+                                                    'custom']), required=True)
+@click.argument('custom_cron', nargs=-1)
+def schedule(nameofcron, tag, cron_schedule, custom_cron):
+    """schedule: Commande pour prévoir une backup régulière
+        --nameofcron: option pour entrer le nom du cron
+        --tag: option pour saisir un tag pour le cron
+        --schedule: option pour choisir le schedule du cron
+        --custom-cron: option pour entrer un cron personnalisé"""
+    click.echo(f"Name of cron : \"{nameofcron}\"")
+    click.echo(f"Cron tag and schedule : \"{tag}\" \"{cron_schedule}\"")
+    if cron_schedule == 'custom':
+        click.echo(f"Custom cron : \"{custom_cron}\"")
+
+
+@autobackup.command()
+@click.option('-n', '--nameofcron', nargs=1, help="entrer le nom du cron à stopper")
+def stop(nameofcron):
+    """schedule: Commande pour stopper une backup régulière
+        --nameofcron: option pour entrer le nom du cron à stopper"""
+    click.echo(f"Stopped cron : \"{nameofcron}\"")
+
+
 def main():
     """main: Fonction principale"""
     kdna()
+
 
 if __name__ == '__main__':
     main()
