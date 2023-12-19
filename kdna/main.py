d52a4cf2bc7de563d660eb5e3d1f5227f27fe2d1
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-19 08:48:21 +0100

    update main for start

--- kdna/main.py
+++ kdna/main.py
@@ -2,7 +2,6 @@ import click
 from commands.backup import backup
 from commands.server import server
 from commands.autobackup import autobackup
-from encrypt.encrypt import encrypt_archive, decrypt_archive
 from encrypt import encrypt
 
 
@@ -31,20 +30,22 @@ def main():
 """
 @click.group()
 def kdna():
-    """kdna : Commande principale"""
+    """"""
+
+
+def main():
     kdna.add_command(backup)
     kdna.add_command(server)
     kdna.add_command(autobackup)
 
-
-def main():
     """main: Fonction principale"""
-    encrypt.load_key()
-    encrypt.cyper()
-    encrypt.decypher()
-    encrypt.cypher_folders("./out", "./encoded")
-    encrypt.decypher_folders("./encoded", "./decoded")
+    ##encrypt.load_key()
+    #encrypt.cypher()
+    #encrypt.decypher()
+    #encrypt.cypher_folders("./out", "./encoded")
+    #encrypt.decypher_folders("./encoded", "./decoded")
     kdna()
+
 
 
 if __name__ == "__main__":
