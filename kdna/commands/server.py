46bf1d213d1410e842a7685d25501e68b2185c53
pauline <pauline.contat00@gmail.com>
2023-12-19 20:03:37 +0100

    Ajout commandes + doc

--- kdna/commands/server.py
+++ kdna/commands/server.py
@@ -1,49 +1,65 @@
 import click
 from kdna.conf_utils.server import Server
-from kdna.server.server_service import ServerService
-
 @click.group()
 def server():
-    """server: Commande pour lancer le serveur"""
+    """Commande pour lancer le serveur"""
 
+#! Création des commandes du groupe server
+# Création de la commande set
 @server.command()
-@click.argument('name')
-@click.option('--alias', is_flag=True, default=False)
-def set(name, alias):
-    """set: Commande pour sélectionner un serveur
-        --name: option pour entrer le nom du serveur"""
+@click.argument('alias', default='')
+@click.argument('id', default='')
+def set(alias, id):
+    """Commande pour sélectionner un serveur.\n
+    param alias: l'alias du serveur à sélectionner\n
+    :type alias: str\n
+    :param id: l'id du serveur à séléctionner\n
+    :type id: str\n
+    :return: un message de confirmation ou d'erreur\n
+    :rtype: str"""
     if alias:
-        click.echo(f"Sélection du server {name} par l'alias")
-    else:
-        click.echo(f"Sélection du server {name} par l'id")
+        click.echo(f"Alias du serveur : \"{alias}\"")
+    if id:
+        click.echo(f"ID du serveur : \"{id}\"")
 
-
+# Création de la commande add
 @server.command()
-@click.argument('name')
-def add(name):
-    #Todo ServerService create
-    click.echo("FAUT IMPLEMENTER LES GARS")
+@click.argument('alias', nargs=1, required=True)
+@click.argument('port', nargs=1, required=True, type=str)
+@click.argument('userip', nargs=1, required=True, type=str)
+def add(alias, port, userip):
+    """Commande pour ajouter un serveur\n:
+    param alias: l'alias du serveur à ajouter\n
+    :type alias: str\n
+    :param port: le port du serveur à supprimer\n
+    :type port: str\n
+    :param userip: l'utilisateur et l'ip du serveur à supprimer --> user@ip\n
+    :type userip: str\n
+    :return: un message de confirmation ou d'erreur\n
+    :rtype: str"""
+    click.echo(f"Added server with alias : \"{alias}\", port : \"{port}\", and user@ip : \"{userip}\"")
 
+# Création de la commande delete
 @server.command()
-@click.argument('id')
-@click.option('--alias', is_flag=True, default=False)
-def delete(id, alias):
-    serverService = ServerService()
-    serverService.delete_server(id, alias)
-    click.echo(f"Id {id} - supprimé par alias : {alias}")
+@click.argument('alias', default='')
+@click.argument('id', default='')
+def delete(alias, id):
+    """Commande pour supprimer un serveur.\n
+    :param alias: l'alias du serveur à supprimer\n
+    :type alias: str\n
+    :param id: l'ID du serveur à supprimer\n
+    :type id: str\n
+    :return: un message de confirmation ou d'erreur\n
+    :rtype: str"""
+    if alias:
+        click.echo(f"Suppression du serveur : \"{alias}\"")
+    if id:
+        click.echo(f"Suppression du serveur : \"{id}\"")
 
+# Création de la commande list
 @server.command()
-def status():
-    click.echo("Server Status")
-
-
-@server.command()
-@click.argument('id')
-@click.option('--address', '-a')
-@click.option('--credentials', '-c')
-@click.option('--port', '-p')
-@click.option('--alias', '-na')
-def update(id, address, credentials, port, alias):
-    click.echo(f"old alias {id}, {address}, {credentials}, {port}, {alias}")
-    serverService = ServerService()
-    serverService.update_server(id, credentials, port, address, alias)
+def list():
+    """Commande pour lister les serveurs\n
+    :return: Liste des serveurs : class: `str`\n
+    :rtype: list"""
+    click.echo(f"List of server : \n...\n...")
