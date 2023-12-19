612095e351d0e5ed51cf2bd98d85705fcde18aca
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2023-12-19 18:49:54 +0100

    implement ssh connection class

--- kdna/connexion_ssh/sshConnect.py
+++ kdna/connexion_ssh/sshConnect.py
@@ -4,21 +4,39 @@ from fabric import Connection
 
 
 class SSHClient:
-    def __init__(self, name,ip) -> None:
+    
+    
+    def __init__(self,name,ip) -> None:
         self.user = name
         self.serverIP= ip
+        self.SSHInstance = None
 
     def getSSHparameter(self):
         return self.user + '@' +self.serverIP
     
     def connect(self):
         try:
-            return Connection(self.getSSHParameter())
+            self.SSHInstance=Connection(self.getSSHParameter())
         except:
-            print("An error as occured: "+"\n{0.stderr}")
+            print("An error as occured on connection")
             return None
         
-    
+    def closeConnection(self):
+        try:
+            self.SSHInstance.close()
+        except:
+            print("An error as occured while closing ssh instance")
+            return None
+            
+    def sendCommand(self,command):
+        try:
+            result = self.SSHInstance.run(command, hide=True)
+            msg = "\n{0.stdout}"
+            print(msg.format(result))
+        except:
+            print("An error as occured: "+ "\n{0.stderr}")
+            return None
+        
 
 
 # sshCLient = SSHConnect("dzada")
