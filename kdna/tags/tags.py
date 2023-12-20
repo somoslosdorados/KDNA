8716b61fa466f4dcbadf2af241841439eb6efa04
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2023-12-20 19:11:45 +0100

    adding logs

--- kdna/tags/tags.py
+++ kdna/tags/tags.py
@@ -14,10 +14,12 @@ def addTags(connexionInstance,project,newTag,newFile):
     with open('tags.conf',"a") as tagFile:
         tagFile.write(newTaggedBackup)
     connexionInstance.put("tags.conf",pathToConfTag)
+    print(f"Le fichier {newFile} est maintenant taggé par {newTag}")
     os.remove('tags.conf')
 
 def deleteTags(connexionInstance,project,oldTag):
     """arguments: oldTag """
+    removed=True
     pathToConfTag="./kdna/"+project+"/tags.conf"
     connexionInstance.get(pathToConfTag)
     with open('tags.conf',"r") as tagFile:
@@ -27,6 +29,14 @@ def deleteTags(connexionInstance,project,oldTag):
             tag=line.split(", ")[0]
             if(tag!=oldTag):
                 tagFile.write(line)
+            else:
+                file=line.split(", ")[1].strip('\n')
+                removed=False
+                break
+    if(removed):
+        print(f"Aucun tag {oldTag} n'a été trouvé pour la supression")
+    else:
+        print(f"Le tag {oldTag} associé au fichier {file} a été supprimé")
     connexionInstance.put("tags.conf",pathToConfTag)
     os.remove('tags.conf')
     return
