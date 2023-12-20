83708713b9f7465fd6344750c8f35b37679deea4
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2023-12-20 08:49:35 +0100

    refacto parsing for other branches

--- kdna/parsing/server.py
+++ kdna/parsing/server.py
@@ -5,25 +5,21 @@ class Server:
     port: int
     alias: str
     '''
-    def __init__(self, id_server: str, credentials: str, port: int, alias: str):
+    def __init__(self, id_server: str, credentials: str, path: str,  port: int, alias: str):
         '''Constructeur de la classe'''
         self.id_server = id_server
         self.credentials = credentials
+        self.path = path
         self.port = port
         self.alias = alias
 
     def __str__(self):
         '''Methode d'affichage de la classe'''
-        return f"Server: {self.id_server} {self.credentials} {self.port} {self.alias}"
+        return f"Server: {self.id_server} {self.credentials} {self.path} {self.port} {self.alias}"
 
 
-class ParseServer:
-    '''Classe permettant de parser un serveur'''
-    def __init__(self, line: str):
-        '''Constructeur de la classe'''
-        self.line = line
-
-    def parse(self) -> Server:
-        '''Methode permettant de parser un serveur'''
-        id_server, credentials, port, alias = self.line.strip().split(', ')
-        return Server(id_server, credentials, port, alias)
+def parseServer(line):
+    '''Methode permettant de parser un serveur'''
+    values = line.strip().split(', ')
+    id_server, credentials, path, port, alias = values
+    return Server(id_server, credentials, path, port, alias)
