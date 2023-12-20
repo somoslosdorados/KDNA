8d36be34a6fa4da331b3cecac24c3e8424b1e5c5
Théo Tchilinguirian <theo.tchlx@gmail.com>
2023-12-20 19:07:16 +0100

    Defined more functions to make crons

---
+++ kdna_service/kdna-service.py
@@ -0,0 +1,38 @@
+
+
+
+# utilise les args de la commande de pauline dans commands/autobackup.py ; si a besoin d'utiliser plus d'args, lui dire (quelle ajoute aux params d ela cmd)
+
+
+import sys # ? needed ? We'll see
+import subprocess # ? to start bash scripts to make crons and stuff
+
+
+def argv_getter():
+    """
+    """
+    # mettre dans un module de fonctions auxilliaires ?
+    nameofcron = sys.argv[1]
+    tag = sys.argv[2]
+    schedule = sys.argv[3]  # pre-made dates at which cron crons
+    custom_cron = sys.argv[4]  # dates at which cron crons
+    if schedule ==
+    return nameofcron, tag, schedule, cron_schedule
+        
+
+
+
+
+def delay_reformatter(delay):
+    """
+    """
+    # mettre dans un module de fonctions auxilliaires ?
+    # récupère la date de l'appel de la commande de paupau et la formatte pour le cron.
+
+
+def cron_igniter(nameofcron:str, tag:str, cron_schedule:str, custom_cron:str):
+    """
+    Starts a cron
+    """
+    command = "./cronstart.sh"
+    subprocess.run(command, shell=True, check=True)
