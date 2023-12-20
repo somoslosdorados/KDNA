84d9dad2b03c75e69690dc07d77c74fe155a4a97
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2023-12-20 18:16:03 +0100

    adding tags

--- kdna/tags/tags.py
+++ kdna/tags/tags.py
@@ -1,21 +1,26 @@
-"""We will use connexion instance gathered by the module"""
+"""We will use connexion instance gathered by the module
+    If the service is running the tags.conf file could change"""
 from fabric import Connection
-import fabric
+import os
 
 client = Connection('test@192.168.122.226')
-def addTags(connexionInstance,path,tag):
-    """argument: ssh instance / path to the tags.conf / name of the tags
+def addTags(connexionInstance,project,newTag,newFile):
+    """arguments: ssh instance / name of the project / name of the tags
     In the file tag.conf we must identify the tag [tags] to write tags at the end 
     of the file"""
-    
-    newTaggedBackup=tag+", "+path.split("/")[-1]
-    print(newTaggedBackup)
-    
+    newTaggedBackup=newTag+", "+newFile+"\n"
+    pathToConfTag="./kdna/"+project+"/tags.conf"
+    connexionInstance.get(pathToConfTag)
+    with open('tags.conf',"a") as tagFile:
+        tagFile.write(newTaggedBackup)
+    connexionInstance.put("tags.conf",pathToConfTag)
+    os.remove('tags.conf')
 
-def deleteTags():
+def deleteTags(connexionInstance,project,oldTag,newFile):
+    """arguments: """
     return
 
 def readTags():
     return
 
-addTags(client,'/kdna/project1/tag.conf','superTag')
+addTags(client,'project1','superTag','hey.zip')
