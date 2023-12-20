782de34e88f556aba93105a342b599fcf99975d7
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-12-20 19:02:29 +0100

    tests + basic encryption module

--- kdna/encrypt/encrypt.py
+++ kdna/encrypt/encrypt.py
@@ -5,25 +5,87 @@ This module contains encryption functions.
 :email: <placeholder>, theo.tchlx@gmail.com
 :date: 2023-12-15
 """
-
-
 import os
-import shutil
+import tarfile
 from cryptography.fernet import Fernet
 
 
-def generate_key():
+kdna_default_path = os.path.join(os.path.expanduser('~'), '.kdna/key.key')
+kdna_path_to_path = os.path.join(os.path.expanduser('~'), '.kdna/path')
+
+
+def generate_key(key_path: str = kdna_default_path) -> str:
+    """
+    Generate a key and save it into a file
+    :param key_path: path to the key file
+    :return: path to the key file
+    """
+    if not os.path.exists(os.path.join(os.path.expanduser('~'), '.kdna')):
+        os.mkdir(os.path.join(os.path.expanduser('~'), '.kdna'))
+
+    if key_path != kdna_default_path:
+        with open(kdna_path_to_path, 'w') as file:
+            file.write(os.path.join(os.getcwd(), key_path))
+
     key = Fernet.generate_key()
-    file = open('key.key', 'wb')
+    file = open(key_path, 'wb')
     file.write(key)
     file.close()
+    return key_path
 
 
-def load_key():
-    file = open('key.key', 'rb')
-    key = file.read()
-    file.close()
+def load_key() -> bytes:
+    """
+    Load the previously generated key
+    :param key_path: path to the key file
+    :return: key as bytes
+    """
+    key_path = os.path.join(os.path.expanduser('~'), '.kdna/key.key')
+    if os.path.exists(kdna_path_to_path):
+        with open(kdna_path_to_path, 'r') as file:
+            key_path = file.read()
+
+    with open(key_path, 'rb') as file:
+        key = file.read()
     return key
+
+
+def backup(path: str, out: str, encrypt: bool) -> str:
+    """
+    :param path: path to folder to backup
+    :param out: path to output file
+    :param encrypt: encrypt the file if True then the output
+    file will be a .enc file else it will be a .tar.gz file
+    :return: path to output file
+    """
+
+    if encrypt:
+        with tarfile.open(out+"_temp.tar.gz", "w:gz") as tar:
+            tar.add(path)
+        cypher(out+"_temp.tar.gz", out+".enc")
+        os.remove(out+"_temp.tar.gz")
+        return out+".tar.gz"
+    with tarfile.open(out+".tar.gz", "w:gz") as tar:
+        tar.add(path)
+
+    return out+".tar.gz"
+
+
+def restore(path: str, out: str) -> str:
+    """
+    :param path: path to .tar.gz or .enc file to restore
+    :param out: path to output folder
+    :param encrypted: is the file encrypted
+    :return: path to output folder
+    """
+    encrypted = path.endswith(".enc")
+    tar_name = path
+    if encrypted:
+        decypher(path, out+".tar.gz")
+        tar_name = out+".tar.gz"
+    file = tarfile.open(tar_name, "r:gz")
+    file.extractall(out)
+    return out+"/"+file.getnames()[0]
 
 
 def cypher(path: str, out: str) -> bytes:
@@ -35,50 +97,9 @@ def cypher(path: str, out: str) -> bytes:
     fer = Fernet(key)
     with open(path, "rb") as f_in, open(out, "wb") as f_out:
         data = f_in.read()
-
         encrypted = fer.encrypt(data)
-        print(encrypted)
-
         f_out.write(encrypted)
     return encrypted
-
-
-def cypher_folders(path: str, out: str):
-    """
-    :param path: path to folder to encrypt
-    :param out: path to output folder
-    """
-    shutil.copytree(path, out)  # copy folder to then encrypt it
-    for dirpath, dirnames, filenames in os.walk(out):
-        for filename in filenames:
-            cypher(os.path.join(dirpath, filename),
-                   os.path.join(dirpath, filename))
-
-
-def decypher_folders(path: str, out: str):
-    """
-    :param path: path to folder to decrypt
-    :param out: path to output folder
-    """
-    shutil.copytree(path, out)  # copy folder to then encrypt it
-    for dirpath, dirnames, filenames in os.walk(out):
-        for filename in filenames:
-            decypher(os.path.join(dirpath, filename),
-                     os.path.join(dirpath, filename))
-
-
-def backup(path: str, out: str):
-    shutil.copytree(path, out)  # copy folder to then encrypt it
-    cypher_folders(path, out)  # encrypt folder
-    shutil.make_archive(out, 'zip', out)  # zip folder
-    shutil.rmtree(out)  # remove folder
-
-
-def restore(path: str, out: str):
-    shutil.unpack_archive(path, out+"_temp")  # unzip folder
-    decypher_folders(out+"_temp", out)  # decrypt folder
-    os.remove(path)
-    shutil.rmtree(out+"_temp")  # remove folder
 
 
 def decypher(path: str, out: str):
@@ -92,7 +113,5 @@ def decypher(path: str, out: str):
         data = f_in.read()
 
     with open(out, "wb") as f_out:
-        print(data)
         decrypted = fer.decrypt(data)
-        print(decrypted)
         f_out.write(decrypted)
---
+++ tests/test_encrypt.py
@@ -0,0 +1,109 @@
+import os
+import pytest
+import shutil
+from kdna.encrypt import encrypt
+
+DATA_PATH = "tests/data"
+
+
+def gen_file_structure_bin(nb_files: int = 10) -> dict:
+    file_and_content = {}
+    print(os.getcwd())
+
+    os.mkdir(DATA_PATH+"/test")
+
+    for k in range(0, nb_files):
+        with open(f"{DATA_PATH}/test/test{k}.txt", "w") as test_file:
+            file_and_content[f"test{k}.txt"] = f"Hello World : {k}!"
+            test_file.write(f"Hello World : {k}!")
+
+    return file_and_content
+
+
+def gen_deeper_file_structure_bin(nb_files: int = 10) -> dict:
+    print(os.getcwd())
+    os.mkdir(DATA_PATH+"/test")
+    os.mkdir(DATA_PATH+"/test/test")
+
+    file_and_content = {}
+
+    path = DATA_PATH+"/test/"
+    for k in range(0, nb_files):
+        if int(nb_files / 2) == k:
+            path += "test/"
+        with open(f"{path}/test{k}.txt", "w") as test_file:
+            file_and_content[f"test{k}.txt"] = f"Hello World : {k}!"
+            test_file.write(f"Hello World : {k}!")
+
+    return file_and_content
+
+
+def delete_file_structure():
+    print(os.getcwd())
+    shutil.rmtree(DATA_PATH+"/test")
+
+
+@pytest.fixture(autouse=True)
+def clean_after_test():
+    yield
+    try:
+        delete_file_structure()
+    except FileNotFoundError:
+        pass
+
+
+def test_backup():
+    gen_file_structure_bin()
+    file_out = encrypt.backup(
+        DATA_PATH+"/test", DATA_PATH+"/test/backup", False)
+    assert os.path.exists(file_out)
+    assert file_out == DATA_PATH+"/test/backup.tar.gz"
+    os.remove(DATA_PATH+"/test/backup.tar.gz")
+
+
+def test_backup_encrypted():
+    gen_file_structure_bin()
+    file_out = encrypt.backup(
+        DATA_PATH+"/test", DATA_PATH+"/test/backup", True)
+    print(file_out)
+    assert os.path.exists(file_out)
+    assert file_out == DATA_PATH+"/test/backup.tar.gz"
+    os.remove(DATA_PATH+"/test/backup.tar.gz")
+
+
+def test_backup_decrypted():
+    backup_content = gen_file_structure_bin()
+    file_out = encrypt.backup(
+        DATA_PATH+"/test", DATA_PATH+"/test/backup", True)
+    restore_out = encrypt.restore(file_out,
+                                  DATA_PATH+"/test/backup_decrypted", True)
+    print("restore_out = " + restore_out)
+    assert os.path.exists(restore_out)
+    assert restore_out == DATA_PATH+"/test/backup_decrypted/tests/data/test"
+    for file in backup_content:
+        with open(restore_out+"/"+file, "r") as f:
+            assert f.read() == backup_content[file]
+
+
+def test_backup_encrypted_deeper():
+    gen_deeper_file_structure_bin()
+    file_out = encrypt.backup(
+        DATA_PATH+"/test", DATA_PATH+"/test/backup", True)
+    print(file_out)
+    assert os.path.exists(file_out)
+    assert file_out == DATA_PATH+"/test/backup.tar.gz"
+    os.remove(DATA_PATH+"/test/backup.tar.gz")
+
+
+def test_backup_decrypted_deeper():
+    backup_content = gen_deeper_file_structure_bin()
+    file_out = encrypt.backup(
+        DATA_PATH+"/test", DATA_PATH+"/test/backup", True)
+    restore_out = encrypt.restore(file_out,
+                                  DATA_PATH+"/test/backup_decrypted", True)
+    print("restore_out = " + restore_out)
+    assert os.path.exists(restore_out)
+    assert restore_out == DATA_PATH+"/test/backup_decrypted/tests/data/test"
+    for file in backup_content:
+        with open(restore_out+"/"+file, "r") as f:
+            assert f.read() == backup_content[file]
