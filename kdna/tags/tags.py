8832dfa9dbe7daaff73b3825258d5fc29ce3d452
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2023-12-20 09:59:16 +0100

    writing comment on add tags

--- kdna/tags/tags.py
+++ kdna/tags/tags.py
@@ -1,6 +1,10 @@
 """We will use connexion instance gathered by the module"""
 
-def addTags():
+def addTags(connexionInstance,path,tags):
+    """argument: ssh instance / path to the tags.conf / name of the tags
+    In the file tag.conf we must identify the tag [tags] to write tags at the end 
+    of the file"""
+    
     return
 
 def deleteTags():
