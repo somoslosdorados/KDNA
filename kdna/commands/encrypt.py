ebee843f2be94d67a1e0490c227d62f928089c82
mathias <mathiasdurat@gmail.com>
2023-12-20 19:41:54 +0100

    cli for encryption done

---
+++ kdna/commands/encrypt.py
@@ -0,0 +1,32 @@
+"""
+This module contains the commands for encryption managing in kdna.
+
+:author: DURAT Mathias
+:email: mathias.durat01@etu.umontpellier.fr
+:date: 2023-12-20
+"""
+import click
+from kdna.conf_utils.server import Server
+@click.group()
+def encrypt():
+    """Command to enable/deactivate encryption and generate encryption key"""
+
+
+@encrypt.command(name='key-gen')
+@click.option('--path', is_flag=True, default=False)
+def keygen(path):
+    """Command to generate encryption key
+    :param path: option to specify the path of the key"""
+    if path:
+        click.echo(f"Key stored to {path}")
+    click.echo(f"Key generated")
+
+@encrypt.command()
+def activate():
+    """Command to enable encryption"""
+    click.echo(f"Encryption activated")
+
+@encrypt.command()
+def deactivate():
+    """Command to disable encryption"""
+    click.echo(f"Encryption deactivated")
