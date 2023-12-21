148621551524f1495bdf5e993e4687389dfebb52
pauline <pauline.contat00@gmail.com>
2023-12-21 14:28:28 +0100

    fix server baptiste

--- kdna/server/server.py
+++ kdna/server/server.py
@@ -1,24 +1,21 @@
 from fabric import Connection
 
-
 def upload_file(connection: Connection, local_path: str, remote_path: str) -> None:
     """
-    Send a file on a specific path in the remote server. Might throw an exception.
+    Send a file on a specific path in the remote server. Migth throw an exception.
     """
     connection.put(local_path, remote=remote_path)
 
-
 def download_file(connection: Connection, local_path: str, remote_path: str) -> None:
     """
-    Receive a file on a specific path from the remote server. Might throw an exception.
+    Receive a file on a specific path from the remote server. Migth throw an exception.
     """
-    connection.get(local_path, remote=remote_path)
+    connection.get(remote_path, local=local_path)
 
-
-def main():
-    with Connection("bbronsin@168.38.112.136") as c:
-        upload_file(c, "/home/baptiste/Bureau/toto.txt", "/kdna/projet1/")
-
+def main ():
+    with Connection("bbronsin@162.38.112.136") as c:
+        upload_file(c, "/toto.txt", "/home/bbronsin/")
+        download_file(c, "/home/baptiste/", "/home/bbronsin/toto1.txt")
 
 if __name__ == '__main__':
     main()
