f8ad69be58ca162d2e05392ed8dd7e806bf251e7
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-12-21 19:25:19 +0100

    add file to server, without tag nor project managment

--- kdna/server/server.py
+++ kdna/server/server.py
@@ -1,11 +1,23 @@
 from fabric import Connection
 from kdna.tags.tags import getFileNameByTag
 
+
+def directory_exists(connection: Connection, path: str) -> bool:
+    """
+    Check if a directory exists on the remote server. Might throw an exception.
+    """
+    return connection.run(f"test -d {path}", warn=True).ok
+
+
 def upload_file(connection: Connection, local_path: str, remote_path: str) -> None:
     """
-    Send a file on a specific path in the remote server. Might throw an exception.
+    Send a file on a specific path in th/e remote server. Might throw an exception.
     """
+    if not directory_exists(connection, remote_path):
+        connection.run(f"mkdir -p {remote_path}")
+
     connection.put(local_path, remote=remote_path)
+
 
 def download_file(connection: Connection, local_path: str, remote_path: str) -> None:
     """
@@ -33,7 +45,5 @@ def find_path(connection: Connection, tag: str, project_name: str) -> str:
         connection.run(f"find ./kdna/{project_name}/{backup_name}")
     except:
         raise Exception(f"Tag {tag} not found in project {project_name}")
-    
-    
     # renvoie le path du backup
     return f"./kdna/{project_name}/{backup_name}"
