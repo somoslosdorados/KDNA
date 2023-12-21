88873c873376f06f9e2a5fba503a912ea7d9216c
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2023-12-21 14:10:53 +0100

    add encrypt to conf of server and parse

--- kdna/parsing/server.py
+++ kdna/parsing/server.py
@@ -5,21 +5,22 @@ class Server:
     port: int
     alias: str
     '''
-    def __init__(self, id_server: str, credentials: str, path: str,  port: int, alias: str):
+    def __init__(self, id_server: str, credentials: str, path: str,  port: int, alias: str, encrypt : bool):
         '''Constructeur de la classe'''
         self.id_server = id_server
         self.credentials = credentials
         self.path = path
         self.port = port
         self.alias = alias
+        self.encrypt = encrypt
 
     def __str__(self):
         '''Methode d'affichage de la classe'''
-        return f"Server: {self.id_server} {self.credentials} {self.path} {self.port} {self.alias}"
+        return f"Server: {self.id_server} {self.credentials} {self.path} {self.port} {self.alias} {self.encrypt}"
 
 
 def parseServer(line):
     '''Methode permettant de parser un serveur'''
     values = line.strip().split(', ')
-    id_server, credentials, path, port, alias = values
-    return Server(id_server, credentials, path, port, alias)
+    id_server, credentials, path, port, alias, encrypt = values
+    return Server(id_server, credentials, path, port, alias, encrypt)
