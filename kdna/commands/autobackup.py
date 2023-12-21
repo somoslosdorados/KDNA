02c91e8f3f3a48159cabb9ea45373d26b135c825
Théo Tchilinguirian <theo.tchlx@gmail.com>
2023-12-21 11:21:01 +0100

    Fixed and upgraded autobackup.py, added interactive custom cron schedule maker

--- kdna/commands/autobackup.py
+++ kdna/commands/autobackup.py
@@ -9,13 +9,11 @@ def autobackup():
 
 def get_type_cron(cron):
     for char in cron:
-        print("caractere"+char) #test pour voir les caractère parcourus
         if char == ':':
             return ':'
         elif char == '-':
             return '-'
-        else:
-            return -1
+    return -1
 
 
 def is_valid_cron(cron):
@@ -24,20 +22,50 @@ def is_valid_cron(cron):
         cron_parts = cron.split(':')
         if len(cron_parts) != 5:
             return False
-        if 0 >= cron_parts[0] <= 59 and 0 >= cron_parts[1] <= 23 and 1 >= cron_parts[2] <= 31 and 1 >= cron_parts[
-            3] <= 12:
+        if 0 >= cron_parts[0] <= 59 and 0 >= cron_parts[1] <= 23 and 1 >= cron_parts[2] <= 31 and 1 >= cron_parts[3] <= 12:
             return True
         return False
     elif get_type_cron(cron) == '-':
             cron_parts = cron.split(':')
             if len(cron_parts) != 5:
                 return False
-            if 0 >= cron_parts[0] <= 59 and 0 >= cron_parts[1] <= 23 and 1 >= cron_parts[2] <= 31 and 1 >= cron_parts[
-                3] <= 12:
+            if 0 >= cron_parts[0] <= 59 and 0 >= cron_parts[1] <= 23 and 1 >= cron_parts[2] <= 31 and 1 >= cron_parts[3] <= 12:
                 return True
             return False
     else:
         return False
+
+
+def get_custom_cron(sched_part_type:str, cond_inf:int, cond_sup:int):
+    """Fonction qui récupère auprès de l'utilisateur un morceau de custom_cron.\n
+    :param sched_part_type: le type de la partie manquante de custom_cron à demander à l'utilisateur (e.g. 'Hour')\n
+    :sched_part_type sched_part_type: str\n
+    :return: le custom_cron partiel\n
+    :rsched_part_type: str
+    """
+    incorrect = True
+    while incorrect:
+        schedule_part = input(f">Entrez une valeur numérique comprise entre {cond_inf} et {cond_sup} (inclus) pour '{sched_part_type}', ou entrez 'help' pour afficher les correspondances : ")
+        if schedule_part.lower() == "help":
+            print("Minute (0 - 59)\nHeure (0 - 23)\nJour du mois (1 - 31)\nMois (1 - 12)\nJour de la semaine (0 - 6) (dimanche est 0)\nNe rien entrer pour ne pas préciser.")
+        elif schedule_part == '' or cond_inf <= int(schedule_part) <= cond_sup:
+            incorrect = False
+        else:
+            print("Entrée invalide, réessayez S.V.P")
+            
+    return schedule_part+':'
+
+
+def concatenate_custom_cron():
+    """
+    """
+    custom_cron = get_custom_cron("Minute", 0, 59)
+    custom_cron += get_custom_cron("Heure", 0, 23)
+    custom_cron += get_custom_cron("Jour du mois", 0, 31)
+    custom_cron += get_custom_cron("Mois", 1, 12)
+    custom_cron += get_custom_cron("Jour de la semaine", 0, 6)
+    <
+    return custom_cron[:-1]  # Suppression du ':' final
 
 
 # Création des commandes du groupe autobackup
@@ -47,8 +75,7 @@ def is_valid_cron(cron):
 @click.option('-i', '--idcron', nargs=1, required=True, help="entrer l'id du cron")
 @click.option('-n', '--nameofcron', nargs=1, required=True, help="entrer le nom du cron")
 @click.option('-t', '--tag', nargs=1, required=True, help="entrer le tag")
-@click.argument('cron_schedule', type=click.Choice(['daily', 'monthly', 'weekly',
-                                                    'custom']), required=True)
+@click.argument('cron_schedule', type=click.Choice(['daily', 'monthly', 'weekly', 'custom']), required=True)
 @click.argument('custom_cron', nargs=-1)
 @click.option('-d', '--date', nargs=1, required=True, help="entrer la date de la première backup [ xxxx-xx-xx ]")
 @click.option('-s', '--server', nargs=1, required=True, help="entrer l'id du serveur")
@@ -76,13 +103,15 @@ def schedule(idcron, nameofcron, tag, cron_schedule, custom_cron, date, server, 
     click.echo(f"Name of cron : \"{nameofcron}\"")
     click.echo(f"Cron tag and schedule : \"{tag}\" \"{cron_schedule}\"")
     if cron_schedule == 'custom':
-        if not custom_cron:
+        if not custom_cron:  # custom_cron n'est pas donné en argument
             click.echo("L'argument custom_cron doit être suivi d'un schedule de cron personnalisé.")
+            custom_cron = concatenate_custom_cron()  # le custom_cron est donc demandé en input interactif
+            print("Custom cron schedule is :", custom_cron)
         else:
-            print("custom"+str(custom_cron)) #test
+            print("Custom cron schedule is :", custom_cron[0]) #test
             print(is_valid_cron(str(custom_cron)))
     else:
-        print(cron_schedule)
+        print("Cron schedule is :", cron_schedule, "(not custom)")
 
 
 # Création de la commande delete
