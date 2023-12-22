1f05bfc9a253ffaaa7d4950d6d548467bc69f502
Thomas <broine.thomas@gmail.com>
2023-12-22 10:58:00 +0100

    find_path for restore

--- kdna/server/server.py
+++ kdna/server/server.py
@@ -1,4 +1,5 @@
 from fabric import Connection
+from kdna.tags.tags import getFileNameByTag
 
 def upload_file(connection: Connection, local_path: str, remote_path: str) -> None:
     """
@@ -11,6 +12,32 @@ def download_file(connection: Connection, local_path: str, remote_path: str) -> 
     Receive a file on a specific path from the remote server. Might throw an exception.
     """
     connection.get(remote_path, local=local_path)
+
+# méthode pour trouver le path d'un backup à partir d'un tag et d'un nom de projet
+def find_path(connection: Connection, tag: str, project_name: str) -> str:
+    """
+    Find a path in the remote server. Might throw an exception.
+    """
+
+    # vérifier que le dossier du projet existe
+    try:
+        connection.run(f"ls ./kdna/{project_name}") 
+    except:
+        raise Exception(f"Project {project_name} not found")
+    
+    # récupérer le nom du backup à partir du tag
+    backup_name = getFileNameByTag(connection, tag, project_name)
+     
+     # vérifie que le backup_name existe dans le projet
+    try:
+        connection.run(f"find ./kdna/{project_name}/{backup_name}")
+    except:
+        raise Exception(f"Tag {tag} not found in project {project_name}")
+    
+    
+    # renvoie le path du backup
+    return f"./kdna/{project_name}/{backup_name}"
+
 
 def main ():
     with Connection("bbronsin@162.38.112.136") as c:
