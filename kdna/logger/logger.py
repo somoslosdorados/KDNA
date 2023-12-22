6860530f163c156cb7c9adcaf96f66bf286b1d8e
Théo Tchilinguirian <theo.tchlx@gmail.com>
2023-12-22 20:18:03 +0100

    added get_path

--- kdna/logger/logger.py
+++ kdna/logger/logger.py
@@ -4,42 +4,50 @@
 """
 
 
-import subprocess
+import os
 import time
-import sys
+
+
+def get_path():
+    """
+    :return: returns the path of the .py file it is executed it.\n
+    :rtype: str
+    """
+    return os.path.dirname(__file__) + '/' + os.path.basename(__file__)
 
 
 def write_log(msg, path, log_path="logs/logs.txt"):
     """
     :param msg: the message to print and log\n
     :type msg: str\n
-    :param path: the path of the file that creates this log\n
-    :type path: str\n
     :param log_path: the path to log to (default is "logs/logs.txt")\n
     :type log_path: str\n
-    :return:\n
-    :rtype:
+    :return: returns file.write output (that is, number of characters written)\n
+    :rtype: int
     """
     lt = time.localtime()  # local time
-    timestamp = tr(lt.tm_year + " " + str(lt.tm_mday) + " " + str(lt.mtm_mon) + str(lt.tm_hour)
-    text = msg+':'+path+':'+
+    timestamp = "on " + lt(lt.tm_year + " " + str(lt.tm_mon) + " " + str(lt.tm_mday) + " at " + str(lt.tm_hour) + "h " + str(lt.min) + "m " + str(lt.sec) + "s")
+    # local time is formatted as follows : "on year month day at hour minute second"
+    text = msg+':'+path+':'+timestamp+'\n'
     with open("logs/logs.txt", 'a') as file:
-        file.write("m")
-        subprocess.run(command, shell=True, check=True)
+        res = file.write(text)
+    return res
 
 
-def printk():
+def printk(msg, log_path):
     """
-    :param msg:\n
-    :type str:\n
-    :param path:\n
-    :type str:\n
-    :param log_path:\n
-    :type str:\n
-    :return:\n
-    :rtype :
+    This function acts 'like' print, but also logs what you printed in the chosen logs file (default is logs/logs.txt").
+    :param msg: the message to print and log\n
+    :type msg: str\n
+    :param log_path: the path to log to (default is "logs/logs.txt")\n
+    :type log_path: str\n
+    :return: returns file.write output (that is, number of characters written)\n
+    :rtype: int
     """
-    write_log(msg:str, path:str, log_path:str)
+    path = get_path()
+    res = write_log(str(msg), path)
+    print(msg)
+    return res
 
 
 def main():
