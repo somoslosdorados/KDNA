d18e1ee857480e95a866593c0c2443dd8f0b09b6
Louis Labeyrie <labeyrielouis@gmail.com>
2024-01-10 10:17:15 +0100

    Remove init.sh and kdnad.service files from root

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /root ; mkdir /root/kdna_service
-mv /main.py /root/kdna_service
-mv /kdnad.service /etc/systemd/system
-
-systemctl daemon-reload
-systemctl enable kdnad.service
-systemctl start kdnad.service
