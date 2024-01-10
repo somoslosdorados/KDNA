17443e544d185d83832a6a9360f60d4fa6ac1799
Louis Labeyrie <labeyrielouis@gmail.com>
2024-01-10 08:56:14 +0100

    Refactor config file handling in Utils class

--- kdna/conf_utils/utils.py
+++ kdna/conf_utils/utils.py
@@ -1,11 +1,10 @@
 """Utils"""
 
-CONFIG_TXT = 'kdna.conf'
-
 
 class Utils:
     """Fonctions utilitaires pour les fichiers de configuration"""
-    config_file = 'kdna.conf'
+
+    config_file = "kdna.conf"
 
     @staticmethod
     def initialize_config_file():
@@ -14,7 +13,7 @@ class Utils:
 
         # On vérifie si le fichier existe déjà
         try:
-            with open(CONFIG_TXT, 'r', encoding="utf-8") as f:
+            with open(config_content, "r", encoding="utf-8") as f:
                 content = f.read()
                 # Si le fichier existe déjà et qu'il est correctement initialisé, on ne fait rien
                 if "[server]" in content and "[auto-backup]" in content:
@@ -26,7 +25,7 @@ class Utils:
 
         # On initialise le contenu du fichier de configuration si le fichier n'existe pas ou
         # s'il n'est pas correctement initialisé
-        with open(CONFIG_TXT, 'w', encoding="utf-8") as f:
+        with open(config_content, "w", encoding="utf-8") as f:
             f.write(config_content)
 
         print("Le fichier de configuration a été initialisé avec succès.")
@@ -43,14 +42,14 @@ class Utils:
     def read_file_lines(filename):
         """Read a line of the file"""
         # Fonction pour lire les lignes d'un fichier
-        with open(filename, 'r', encoding="utf-8") as f:
+        with open(filename, "r", encoding="utf-8") as f:
             return f.readlines()
 
     @staticmethod
     def write_file_lines(filename, lines):
         """Write a line in the config file"""
         # Fonction pour écrire les lignes dans un fichier
-        with open(filename, 'w', encoding="utf-8") as f:
+        with open(filename, "w", encoding="utf-8") as f:
             f.writelines(lines)
 
     @staticmethod
