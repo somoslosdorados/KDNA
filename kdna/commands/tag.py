ce4374dc45796f915e1ab4651ba48940bdaf6b7a
pauline <pauline.contat00@gmail.com>
2024-01-11 10:54:56 +0100

    README + implémentation commandes tags chgmt nargs update de backup et autobackup

---
+++ kdna/commands/tag.py
@@ -0,0 +1,57 @@
+"""
+Groupe de commandes tag qui permet de gérer les tags
+add: Commande pour créer un tag
+list: Commande pour lister les tags
+"""
+
+import click
+from kdna.server.server_service import ServerService
+from kdna.parsing.parser import listServers
+from tabulate import tabulate
+from kdna.ssh.ssh_client import SSHClient
+from kdna.tags import tags
+ssh = SSHClient("test@debian12.local",[])
+ssh.connect()
+
+
+@click.group()
+def tag():
+    """Commande pour gérer les tags"""
+
+
+#! Création des commandes du groupe tag
+# Création de la commande add
+@tag.command()
+@click.option('-p', '--project', required=True, help="entrer le projet dans lequel ajouter le tag")
+@click.option('-t', '--new_tag', required=True, help="entrer le tag à ajouter")
+@click.option('-f', '--file_to_tag', required=True, help="entrer le fichier à tagger")
+def add(project, new_tag, file_to_tag):
+    """Commande pour ajouter un tag."""
+
+#serversCredential = listServers[0].credentials
+ #   instance = SSHClient(serversCredential).connect()
+
+    try:
+        tags.add_tags(ssh.connection, project, new_tag, file_to_tag, True)
+    except FileNotFoundError as exc:
+        click.echo("Le fichier n'a pas été trouvé")
+    except PermissionError as exc:
+        click.echo("Vous n'avez pas les droits")
+    except Exception as e:
+        print("error = "+e.__str__())
+
+
+# Création de la commande list
+@tag.command()
+@click.option('-p', '--project', required=True, help="entrer le projet du tag à supprimer")
+def list(project):
+    """Commande pour lister les tags."""
+
+    try:
+        tags.read_tags(ssh.connection, project)
+    except FileNotFoundError as exc:
+        click.echo("Le fichier n'a pas été trouvé")
+    except PermissionError as exc:
+        click.echo("Vous n'avez pas les droits")
+    except Exception as e:
+        print("error = "+e.__str__())
--- kdna/main.py
+++ kdna/main.py
@@ -2,6 +2,7 @@ import click
 from kdna.commands.backup import backup
 from kdna.commands.server import server
 from kdna.commands.autobackup import autobackup
+from kdna.commands.tag import tag
 from kdna.encrypt import encrypt
 from kdna.conf_utils.utils import Utils
 
@@ -14,6 +15,7 @@ def main():
     kdna.add_command(backup)
     kdna.add_command(server)
     kdna.add_command(autobackup)
+    kdna.add_command(tag)
 
     """main: Fonction principale"""
     kdna()
