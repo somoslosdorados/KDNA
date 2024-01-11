03e90e17b612fd969cbaae79b9c5567fb1904464
pauline <pauline.contat00@gmail.com>
2024-01-11 09:15:27 +0100

    commentaires + doc

--- kdna/commands/backup.py
+++ kdna/commands/backup.py
@@ -1,3 +1,12 @@
+"""
+Groupe de commandes auto-backup qui permet de gérer les backups
+add : Commande pour créer une backup
+delete : Commande pour supprimer une backup
+update : Commande pour mettre à jour une backup
+restore : commande pour restaurer une backup
+list : Commande pour lister les backups
+"""
+
 import click
 import uuid
 import os
@@ -14,11 +23,18 @@ from kdna.read_backups.agent import list_backups
 
 @click.group()
 def backup():
-    """Commande pour sauvegarder un fichier"""
+    """Commande pour sauvegarder un fichier ou un dossier"""
 
 
 # Création de la fonction display pour afficher le contenu d'un fichier
 def display(path):
+    """
+    Fonction pour afficher le contenu d'un fichier\n
+    :param path: le path du fichier à afficher\n
+    :type path: str\n
+    :return: le contenu du fichier\n
+    :rtype: str
+    """
     try:
         with open(path, mode="r", encoding="utf-8") as file:
             return file.read()
@@ -37,6 +53,13 @@ def display(path):
 @click.argument('path', nargs=1, required=True)
 @click.argument('tag', nargs=1, required=True)
 def add(project, path, tag):
+    """
+    Commande pour créer une backup.\n
+    Arguments obligatoires :\n
+    \t- <project>: le nom du projet à sauvegarder\n
+    \t- <path>: le chemin du fichier ou du dossier à sauvegarder\n
+    \t- <tag>: le tag de la sauvegarde
+    """
     click.echo(f"Creating backup \"{project}\":\n{tag}")
     uuid_backup = str(uuid.uuid4())
     name_of_temp_backup = encrypt.package(path, uuid_backup, kdna_path, True)
@@ -45,6 +68,10 @@ def add(project, path, tag):
     serversCredential = listServers[0].credentials
     try:
         instance = SSHClient(serversCredential).connect()
+    except FileNotFoundError as exc:
+        click.echo("Le fichier n'a pas été trouvé")
+    except PermissionError as exc:
+        click.echo("Vous n'avez pas les droits")
     except Exception as e:
         print("error2 = "+e.__str__())
 
@@ -54,15 +81,18 @@ def add(project, path, tag):
         os.remove(path_to_local_backup)
         tags.addTags(instance.connection, "project",
                      tag, name_of_temp_backup, True)
+    except FileNotFoundError as exc:
+        click.echo("Le fichier n'a pas été trouvé")
+    except PermissionError as exc:
+        click.echo("Vous n'avez pas les droits")
     except Exception as e:
         print("error = "+e.__str__())
 
 # Création de la commande delete
-
-
 @backup.command()
 @click.option('-t', 'pathtag', nargs=1, required=True, help="entrer le path du fichier et le tag [ path:tag ]")
 def delete(pathtag):
+    """Commande pour supprimer une backup.\n"""
     click.echo(f"Suppression du fichier : \"{pathtag}\"")
 
 
@@ -70,6 +100,9 @@ def delete(pathtag):
 @backup.command()
 @click.argument('project_name', nargs=1, required=True)
 def list(project_name):
+    """Commande pour lister les backups d'un projet\n
+    Argument obligatoire :\n
+    \t- <project_name>: le nom du projet pour lequel lister les backups"""
     try:
         instance = SSHClient(listServers[0].credentials).connect()
     except Exception as e:
@@ -96,6 +129,9 @@ def list(project_name):
               help="entrer le nom du fichier à restaurer et le tag [ name:tag ]")
 @click.argument('path', nargs=1, required=True)
 def restore(nametag, path):
+    """Commande pour restaurer une backup.\n
+        Argument obligatoire :\n
+        \t- <path>: le chemin du fichier ou du dossier à restaurer\n"""
     click.echo(f"Restauration du fichier : \"{nametag}\"")
 
     try:
