7d74154b2c11e293f39a2f3137d78fceaea52115
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-01-11 18:59:45 +0100

    ref tagz to do: verif if tagz exit

--- kdna/tags/tags.py
+++ kdna/tags/tags.py
@@ -6,9 +6,16 @@
 import os
 import time
 from fabric import Connection
+from kdna.connexion_ssh.sshConnect import SSHClient
 
+def backup_exist(connection_instance: Connection,project: str,file_to_tag: str):
+        # Vérification que la sauvegarde existe sur le serveur
+    try:
+        connection_instance.run(f"find ./kdna/{project}/{file_to_tag}",hide=True)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Erreur: la sauvegarde {file_to_tag} n'existe pas")
 
-def add_tags(connexion_instance: Connection, project: str, new_tag: str, file_to_tag: str, verbose=False):
+def add_tags(connection_instance: Connection, project: str, new_tag: str, file_to_tag: str, verbose=False):
     """arguments: ssh instance / name of the project / name of the tags
     In the file tag.conf we must identify the tag [tags] to write tags at the end 
     of the file
@@ -17,58 +24,25 @@ def add_tags(connexion_instance: Connection, project: str, new_tag: str, file_to
 
     new_tagged_backup = new_tag+", "+file_to_tag+"\n"
     path_to_conf_tag = "./kdna/"+project+"/tags.conf"
+    #VERIFIER QUE LES TAGS NE SONT PAS EXISTANT !!!!!!!!!!!!!
+    #Vérification que la backup existe sur le serveur
+    backup_exist(connection_instance,project,file_to_tag)
 
-    # Récupération en local du fichier de config sur les tags
-    get_tag_conf(path_to_conf_tag, connexion_instance)
-
-    # Ouverture du fichier de conf en lecture
+    #Ecrire dans le fichier tag.conf le nouveau tag
     try:
-        with open('tags.conf', "r") as tag_file:
-            tag_lines = tag_file.readlines()
-        tag_lines.pop(0)
-    except:
+        connection_instance.run(f"echo {new_tagged_backup} >> {path_to_conf_tag}")
+    except PermissionError:
         raise PermissionError("Erreur de permission: read sur tags.conf")
 
-    # Vérification que le tag n'est pas déjà utilisé
-    for line in tag_lines:
-        tag_file_couple = line.split(", ")
-        if (new_tag == tag_file_couple[0]):
-            os.remove('tags.conf')
-            raise FileExistsError(f"Le tag {tag_file_couple[0]} est déjà associé la backup {tag_file_couple[1]}")
 
-    # Vérification que la sauvegarde existe sur le serveur
-    try:
-        c = connexion_instance.run(f"find ./kdna/{project}/{file_to_tag}")
-    except:
-        raise FileNotFoundError(f"Erreur: la sauvegarde {file_to_tag} n'existe pas")
-
-    # Ouverture du fichier de conf en append
-    try:
-        with open('tags.conf', "a") as tag_file:
-            tag_file.write(new_tagged_backup)
-    except:
-        raise PermissionError("Erreur de permission: append sur tags.conf")
-
-    # Renvoie du fichier sur le serveur
-    try:
-        connexion_instance.put("tags.conf", path_to_conf_tag)
-    except:
-        raise PermissionError("Erreur lors du renvoie du fichier sur le serveur")
-
-    # Print+clean du fichier local
-    if (verbose):
-        print(f"Le fichier {file_to_tag} est maintenant taggé par {new_tag}")
-    os.remove('tags.conf')
-
-
-def delete_tags(connexion_instance: Connection, project: str, oldTag: str, verbose=False):
+def delete_tags(connection_instance: Connection, project: str, oldTag: str, verbose=False):
     """arguments: oldTag """
 
     # Validateur
     removed = False
     path_to_conf_tag = "./kdna/"+project+"/tags.conf"
     # Récupération en local du fichier de config sur les tags
-    get_tag_conf(path_to_conf_tag, connexion_instance)
+    get_tag_conf(path_to_conf_tag, connection_instance)
 
     # Ouverture du fichier de conf en lecture
     try:
@@ -92,38 +66,38 @@ def delete_tags(connexion_instance: Connection, project: str, oldTag: str, verbo
 
     if (not removed):
         os.remove('tags.conf')
-        raise FileNotFoundError(f"Aucun tag {oldTag} n'a été trouvé pour la suppression")
+        raise FileNotFoundError(f"Aucun tag {oldTag} n'a été trouvé pour la supression")
     else:
         if (verbose):
             print(f"Le tag {oldTag} associé au fichier {file} a été supprimé")
 
     try:
-        connexion_instance.put("tags.conf", path_to_conf_tag)
+        connection_instance.put("tags.conf", path_to_conf_tag)
         os.remove('tags.conf')
     except:
         os.remove('tags.conf')
         raise PermissionError("La deletion de tag n'a pas été appliqué sur le server")
 
 
-def update_tags(connexion_instance: Connection, project: str, oldTag: str, new_tag: str, file_to_tag: str, verbose=False):
+def update_tags(connection_instance: Connection, project: str, oldTag: str, new_tag: str, file_to_tag: str, verbose=False):
     try:
-        delete_tags(connexion_instance, project, oldTag)
+        delete_tags(connection_instance, project, oldTag)
     except:
         raise PermissionError("Une erreur est survenue lors de la tentative de suppression du tag")
 
     try:
-        add_tags(connexion_instance, project, new_tag, file_to_tag)
+        add_tags(connection_instance, project, new_tag, file_to_tag)
     except:
         raise PermissionError("Une erreur est survenue lors de la tentative de création du tag")
     if (verbose):
         print(f"Le tag {oldTag} a été modifié en {new_tag}")
 
 
-def read_tags(connexion_instance: Connection, project: str):
+def read_tags(connection_instance: Connection, project: str):
     path_to_conf_tag = "./kdna/"+project+"/tags.conf"
 
     # Récupération en local du fichier de config sur les tags
-    get_tag_conf(path_to_conf_tag, connexion_instance)
+    get_tag_conf(path_to_conf_tag, connection_instance)
 
     # Sélection des couples Tag, Fichier
     with open('tags.conf', "r") as tag_file:
@@ -137,11 +111,11 @@ def read_tags(connexion_instance: Connection, project: str):
         print('{:>8} {:>8}'.format(tag_file_couple[0], tag_file_couple[1]))
     os.remove('tags.conf')
 
-def get_file_name_by_tag(connexion_instance: Connection,project: str,tag:str,verbose: True):
+def get_file_name_by_tag(connection_instance: Connection,project: str,tag:str,verbose: True):
     path_to_tag=f"./kdna/{project}/tags.conf"
     found = False
     # récupération du fichier tag.conf
-    get_tag_conf(path_to_tag, connexion_instance)
+    get_tag_conf(path_to_tag, connection_instance)
     # Recherche dans le fichier si un tag correspond a un fichier
     with open("tags.conf", "r") as tag_file:
 
@@ -156,7 +130,7 @@ def get_file_name_by_tag(connexion_instance: Connection,project: str,tag:str,ver
         raise FileNotFoundError(f"Aucun fichier n'a été associé au tag {tag} dans le project {project}")
 
 
-def get_tag_conf(path_to_conf_tag: str, connexion_instance: Connection):
+def get_tag_conf(path_to_conf_tag: str, connection_instance: Connection):
     try:
         connexion_instance.get(path_to_conf_tag)
     except FileNotFoundError:
