d43645e0570455537cf8cabf109a2f3a332abce2
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-01-15 13:17:55 +0100

    added to main

--- kdna/__main__.py
+++ kdna/__main__.py
@@ -3,6 +3,8 @@ from kdna.commands.backup import backup
 from kdna.commands.server import server
 from kdna.commands.autobackup import autobackup
 from kdna.commands.encrypt import encrypt
+from kdna.commands.tag import tag
+
 @click.group()
 def kdna():
     """"""
@@ -11,4 +13,5 @@ kdna.add_command(backup)
 kdna.add_command(server)
 kdna.add_command(autobackup)
 kdna.add_command(encrypt)
+kdna.add_command(tag)
 
