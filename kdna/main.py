fd454f4fa257bdd0376d028db283ddb216dda4a2
mathias <mathiasdurat@gmail.com>
2024-01-15 10:24:05 +0100

    :build mise en place de l'install avec pip de l'executable kdna
    install possible une fois clone avec pip install . et depuis le repo avec
    pip install -e "git+https://github.com/somoslosdorados/KDNA.git/#egg=kdna"

--- kdna/main.py
+++ kdna/main.py
@@ -2,35 +2,13 @@ import click
 from kdna.commands.backup import backup
 from kdna.commands.server import server
 from kdna.commands.autobackup import autobackup
-from kdna.commands.tag import tag
-from kdna.conf_utils.utils import Utils
 from kdna.commands.encrypt import encrypt
-from kdna.parsing.parser import parseConfig
-from kdna.commands.tag import tag
-from kdna.conf_utils.utils import Utils
-from kdna.commands.encrypt import encrypt
-from kdna.parsing.parser import parseConfig
-
 @click.group()
 def kdna():
     """"""
 
+kdna.add_command(backup)
+kdna.add_command(server)
+kdna.add_command(autobackup)
+kdna.add_command(encrypt)
 
-def main():
-    Utils.initialize_config_file()
-    kdna.add_command(backup)
-    kdna.add_command(server)
-    kdna.add_command(autobackup)
-    kdna.add_command(tag)
-    kdna.add_command(encrypt)
-    parseConfig()
-    kdna.add_command(tag)
-    kdna.add_command(encrypt)
-    parseConfig()
-
-    """main: Fonction principale"""
-    kdna()
-
-
-if __name__ == "__main__":
-    main()
